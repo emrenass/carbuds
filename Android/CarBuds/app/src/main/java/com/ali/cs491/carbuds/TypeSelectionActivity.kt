cca5a5270b5b6f15a8cf116e10689038cae38f54
aocet <alctn33@gmail.com>
2018-10-12 22:37:46 +0300

    eksik dosyalar eklendi

---
+++ Android/CarBuds/app/src/main/java/com/ali/cs491/carbuds/TypeSelectionActivity.kt
@@ -0,0 +1,12 @@
+package com.ali.cs491.carbuds
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TypeSelectionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_type_selection)
+    }
+}
