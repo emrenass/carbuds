4ab507a5f86273b21caa176846bbc5656ff5d25f
emrenas <emre.nas@selvitechnology.com>
2018-10-27 20:51:08 +0300

    Initial database commit

--- Backend_API/database/database.py
+++ Backend_API/database/database.py
@@ -13,24 +13,16 @@ def connect():
     """ Connect to the PostgreSQL database server """
     global conn
     try:
-        # read connection parameters
-
-        # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(host=DB_HOST, database=DB_NAME, user=DB_USERNAME, password=DB_PASS)
+        conn = psycopg2.connect(host=DB_HOST, port=DB_PORT, database=DB_NAME, user=DB_USERNAME, password=DB_PASS)
 
-        # create a cursor
         cur = conn.cursor()
 
-        # execute a statement
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
-
-        # display the PostgreSQL database server version
-        db_version = cur.fetchone()
-        print(db_version)
-
-        # close the communication with the PostgreSQL
+        commands = create_table()
+        for values in commands.values():
+            print(values)
+            cur.execute(values)
+        conn.commit()
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
@@ -38,6 +30,23 @@ def connect():
         if conn is not None:
             conn.close()
             print('Database connection closed.')
+
+def create_table():
+    User = """ CREATE TABLE "user" (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(50),
+                lastname VARCHAR(50),
+                username VARCHAR(30),
+                password VARCHAR(50),
+                registration_date date,
+                email VARCHAR(100)
+                )
+        """
+
+    commands = {
+        "User": User
+    }
+    return commands
 
 
 if __name__ == '__main__':
