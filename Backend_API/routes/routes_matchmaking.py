990f82868ec990c9cdbdf261a6e462d273d62ecc
Dogukan <dogukan.altay@selvitechnology.com>
2018-11-05 17:44:03 +0300

    matchmaking in progress

--- Backend_API/routes/routes_matchmaking.py
+++ Backend_API/routes/routes_matchmaking.py
@@ -12,22 +12,22 @@ route_matchmaking = Blueprint('route_matchmaking', __name__)
 
 @route_matchmaking.route('/set_trip_driver', methods=['POST'])
 def set_trip_driver():
+    user_id = 1
+    start_lat, start_lon = tuple(request.form['trip_start_point'].split(','))
+    end_lat, end_lon = tuple(request.form['trip_end_point'].split(','))
+    trip_start_time = request.form['trip_start_time']
+    available_seat = request.form['available_seat']
     direction_api_key = app.config["DIRECTIONS_API_KEY"]
     directions_base_url = 'https://maps.googleapis.com/maps/api/directions/json'
     url = directions_base_url + '?' + urllib.parse.urlencode({
-        'origin': "%s,%s" % ("38.453", "45.7654"),
-        'destination': "%s,%s" % ("38.543", "45.7642"),
+        'origin': "%s,%s" % (start_lat, start_lon),
+        'destination': "%s,%s" % (end_lat, end_lon),
         'key': direction_api_key,
     })
 
     response = requests.get(url)
     result = dict(response.json())
 
-    user_id = 1
-    start_lat, start_lon = tuple(request.form['trip_start_point'].split(','))
-    end_lat, end_lon = tuple(request.form['trip_start_point'].split(','))
-    trip_start_time = request.form['trip_start_time']
-    available_seat = request.form['available_seat']
     route_polyline = result['routes'][0]['overview_polyline']['points']
 
     query = """INSERT INTO driver_matchmaking_pool 
@@ -51,21 +51,22 @@ def set_trip_driver():
 
 @route_matchmaking.route('/set_trip_hitchhiker', methods=['POST'])
 def set_trip_hitchhiker():
+    user_id = 1
+    start_lat, start_lon = tuple(request.form['trip_start_point'].split(','))
+    end_lat, end_lon = tuple(request.form['trip_end_point'].split(','))
+    trip_start_time = request.form['trip_start_time']
+
     direction_api_key = app.config["DIRECTIONS_API_KEY"]
     directions_base_url = 'https://maps.googleapis.com/maps/api/directions/json'
     url = directions_base_url + '?' + urllib.parse.urlencode({
-        'origin': "%s,%s" % ("38.453", "45.7654"),
-        'destination': "%s,%s" % ("38.543", "45.7642"),
+        'origin': "%s,%s" % (start_lat, start_lon),
+        'destination': "%s,%s" % (end_lat, end_lon),
         'key': direction_api_key,
     })
 
     response = requests.get(url)
     result = dict(response.json())
 
-    user_id = 1
-    start_lat, start_lon = tuple(request.form['trip_start_point'].split(','))
-    end_lat, end_lon = tuple(request.form['trip_start_point'].split(','))
-    trip_start_time = request.form['trip_start_time']
     route_polyline = result['routes'][0]['overview_polyline']['points']
 
     query = """INSERT INTO hitchhiker_matchmaking_pool 
@@ -94,6 +95,25 @@ def check_polylines_intersections(driver_poly, hitchikker_poly):
 
 
 
-@route_matchmaking.route('/get_destination', methods=['POST'])
-def get_destination():
+@route_matchmaking.route('/get_trip_hitchhiker', methods=['POST'])
+def get_trip_hitchhiker():
     user_id = 1
+    start_lat, start_lon = tuple(request.form['trip_start_point'].split(','))
+    end_lat, end_lon = tuple(request.form['trip_start_point'].split(','))
+    trip_start_time = request.form['trip_start_time']
+    # route_polyline = request.form['route_polyline']
+
+    query = """SELECT * 
+                FROM driver_matchmaking_pool
+                WHERE trip_start_point = point('%f', '%f') AND 
+                trip_end_point = point('%f', '%f') """\
+            % (float(start_lat), float(start_lon),
+               float(end_lat), float(end_lon))
+
+    conn = db_connection()
+    result = execute_query(query, conn)
+
+    if result:
+        return jsonify(result)
+    else:
+        return jsonify(False)
