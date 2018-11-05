f4323925df840ba802cc76d1bde2cf5818f97297
Dogukan <dogukan.altay@selvitechnology.com>
2018-11-05 16:10:16 +0300

    Google Directions API added to set_trip functions.

--- Backend_API/routes/routes_matchmaking.py
+++ Backend_API/routes/routes_matchmaking.py
@@ -2,10 +2,10 @@ from flask import request, make_response, render_template, Blueprint, jsonify, r
 from flask import current_app as app
 from Backend_API.utils.decorators import login_required
 from Backend_API.database.database_interface import *
-from Backend_API.init_app import app
 import json
 import time
-import urllib
+import urllib.parse
+import requests
 import urllib3
 
 
@@ -16,27 +16,30 @@ route_matchmaking = Blueprint('route_matchmaking', __name__)
 @route_matchmaking.route('/set_trip_driver', methods=['POST'])
 def set_trip_driver():
     direction_api_key = app.config["DIRECTIONS_API_KEY"]
-    directions_base_url = 'http://maps.googleapis.com/maps/api/directions/json'
-    url = directions_base_url + '?' + urllib.urlencode({
+    directions_base_url = 'https://maps.googleapis.com/maps/api/directions/json'
+    url = directions_base_url + '?' + urllib.parse.urlencode({
         'origin': "%s,%s" % ("38.453", "45.7654"),
         'destination': "%s,%s" % ("38.543", "45.7642"),
         'key': direction_api_key,
     })
 
-    response = str(urllib3.urlopen(url).read())
-    result = json.loads(response.replace('\\n', ''))
+    response = requests.get(url)
+    result = dict(response.json())
 
     user_id = 1
     start_lat, start_lon = tuple(request.form['trip_start_point'].split(','))
     end_lat, end_lon = tuple(request.form['trip_start_point'].split(','))
     trip_start_time = request.form['trip_start_time']
     available_seat = request.form['available_seat']
+    route_polyline = result['routes'][0]['overview_polyline']['points']
 
     query = """INSERT INTO driver_matchmaking_pool 
-                    (user_id, trip_start_point, trip_end_point, available_seat, trip_start_time)
-                    VALUES ('%s', point('%f', '%f'), point('%f', '%f'), '%s', '%s')""" \
+                    (user_id, trip_start_point, trip_end_point, destination_polyline, available_seat, trip_start_time)
+                    VALUES ('%s', point('%f', '%f'), point('%f', '%f'), '%s', '%s', '%s')""" \
             % (user_id, float(start_lat), float(start_lon),
-               float(end_lat), float(end_lon), available_seat, trip_start_time)
+               float(end_lat), float(end_lon),
+               route_polyline,
+               available_seat, trip_start_time)
 
     conn = db_connection()
 
@@ -51,16 +54,30 @@ def set_trip_driver():
 
 @route_matchmaking.route('/set_trip_hitchhiker', methods=['POST'])
 def set_trip_hitchhiker():
+    direction_api_key = app.config["DIRECTIONS_API_KEY"]
+    directions_base_url = 'https://maps.googleapis.com/maps/api/directions/json'
+    url = directions_base_url + '?' + urllib.parse.urlencode({
+        'origin': "%s,%s" % ("38.453", "45.7654"),
+        'destination': "%s,%s" % ("38.543", "45.7642"),
+        'key': direction_api_key,
+    })
+
+    response = requests.get(url)
+    result = dict(response.json())
+
     user_id = 1
     start_lat, start_lon = tuple(request.form['trip_start_point'].split(','))
     end_lat, end_lon = tuple(request.form['trip_start_point'].split(','))
     trip_start_time = request.form['trip_start_time']
+    route_polyline = result['routes'][0]['overview_polyline']['points']
 
     query = """INSERT INTO hitchhiker_matchmaking_pool 
-                        (user_id, trip_start_point, trip_end_point, trip_start_time)
-                        VALUES ('%s', point('%f', '%f'), point('%f', '%f'), '%s')""" \
-            % (user_id, float(start_lat), float(start_lon),
-               float(end_lat), float(end_lon), trip_start_time)
+                        (user_id, trip_start_point, trip_end_point, trip_start_time, destination_polyline)
+                        VALUES ('%s', point('%f', '%f'), point('%f', '%f'), '%s', '%s')""" \
+            % (user_id,
+               float(start_lat), float(start_lon),
+               float(end_lat), float(end_lon),
+               trip_start_time, route_polyline)
 
     conn = db_connection()
 
