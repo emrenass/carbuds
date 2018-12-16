176e48f9677ab2978e53793b7305cf8cee47c41e
aocet <alctn33@gmail.com>
2018-12-16 19:39:24 +0300

    notification

--- Android/CarBuds/app/build/generated/source/buildConfig/debug/com/ali/cs491/carbuds/BuildConfig.java
+++ Android/CarBuds/app/build/generated/source/buildConfig/debug/com/ali/cs491/carbuds/BuildConfig.java
@@ -5,7 +5,7 @@ package com.ali.cs491.carbuds;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.ali.cs491.carbuds";
+  public static final String APPLICATION_ID = "carbuds.fcm_test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
