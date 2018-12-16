815e57f8b327f6b5f09795a8bb4c6390e5b050df
ArasHeper <arasheper@gmail.com>
2018-12-16 23:52:29 +0300

    Register_UI added
    instead of SignUp

--- Android/CarBuds/app/src/main/java/com/ali/cs491/carbuds/MainActivity.java
+++ Android/CarBuds/app/src/main/java/com/ali/cs491/carbuds/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
                 startActivity(intent);
             }
         });
