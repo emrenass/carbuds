a8a1a5fbd075c9811bb26eeaade75c3d04c232e1
aocet <alctn33@gmail.com>
2018-12-16 18:24:52 +0300

    merged changes

--- Android/CarBuds/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ Android/CarBuds/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f090025;
-        public static final int end = 0x7f090048;
-        public static final int gone = 0x7f090056;
-        public static final int invisible = 0x7f090064;
-        public static final int left = 0x7f090069;
-        public static final int packed = 0x7f090084;
-        public static final int parent = 0x7f090086;
-        public static final int percent = 0x7f09008a;
-        public static final int right = 0x7f09008f;
-        public static final int spread = 0x7f0900b6;
-        public static final int spread_inside = 0x7f0900b7;
-        public static final int start = 0x7f0900bc;
-        public static final int top = 0x7f0900d3;
-        public static final int wrap = 0x7f0900e3;
+        public static final int end = 0x7f090049;
+        public static final int gone = 0x7f090057;
+        public static final int invisible = 0x7f090065;
+        public static final int left = 0x7f09006a;
+        public static final int packed = 0x7f090085;
+        public static final int parent = 0x7f090087;
+        public static final int percent = 0x7f09008b;
+        public static final int right = 0x7f090092;
+        public static final int spread = 0x7f0900b9;
+        public static final int spread_inside = 0x7f0900ba;
+        public static final int start = 0x7f0900bf;
+        public static final int top = 0x7f0900d6;
+        public static final int wrap = 0x7f0900e6;
     }
     public static final class styleable {
         private styleable() {}
